6a21f9c07d1e1e2f35578b9521180c9d33851ea3
FrancineMaria <akfrancinemaria1@gmail.com>
2024-05-28 10:33:28 +0000

    Batcher function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from time import time
 from prisma import Prisma
 
-from prisma_connect import connect_to_prisma
+from prisma_connect import batch_operations, connect_to_prisma
 from time_tracker import seconds_to_microseconds
 
 prisma = Prisma()
@@ -16,7 +16,6 @@ async def main():
     start = time()
     async with Prisma() as client:
         for i in range(10000):
-            start2 = time()
             await client.audit_log.create(
                 data={
                     'timestamp': datetime.now(),
@@ -25,21 +24,16 @@ async def main():
                     'details': str(i)
                 }
             )
-        start3 = time()
 
-    print("Mutation Client Start 1",time()-start)
-    # print("Mutation Client Start 2", seconds_to_microseconds(start3-start2))
-    # print("Mutation Start 3", start3)
+    print("Mutation Client Start 1", time()-start)
 
 
 async def main_queries():
     async with Prisma() as client:
         start = time()
         await client.audit_log.find_many()
-    start2 = time()
 
     print("Query Client Start 1", time()-start)
-    # print("Query Client Start 1", start2)
 
 #################### Backend Batcher ####################
 
@@ -49,7 +43,6 @@ async def batcher():
     async with Prisma() as client:
         async with client.batch_() as batcher:
             for i in range(10000):
-                start2 = time()
                 batcher.audit_log.create(
                     data={
                         'timestamp': datetime.now(),
@@ -58,11 +51,27 @@ async def batcher():
                         'details': str(i)
                     }
                 )
-        start3 = datetime.now()
 
     print("Batcher Mutations Start 1", time()-start)
-    # print("Batcher Mutations Start 2", seconds_to_microseconds(start3-start2))
-    # print("Batcher Mutations Start 3", start3)
+
+#################### Backend Batcher with retries ####################
+async def batcher_function():
+    start = time()
+    if await connect_to_prisma(prisma):
+        for i in range(10000):
+            data = [{
+                'timestamp': datetime.now(),
+                'userId': i,
+                'action': str(i),
+                'details': str(i)
+            }]
+            print(type(data))
+            batch_success = await batch_operations(prisma, data)    
+            if batch_success:
+                print("Batch create operations completed successfully.")
+            else:
+                print("Batch create operations failed.")  
+        print("Batcher Mutations Start 1", time()-start)
 
 
 async def batcher_queries():
@@ -81,7 +90,6 @@ async def batcher_queries():
 async def test():
     start1 = time()
     if await connect_to_prisma(prisma):
-        start2 = datetime.now()
         for i in range(10000):
             await prisma.audit_log.create(
                 data={
@@ -91,27 +99,21 @@ async def test():
                     'details': str(i)
                 }
             )
-        start3 = datetime.now()
         print("Test Mutation 1", time()-start1)
-        # print("Test Mutation 2", start2)
-        # print("Test Mutation 3", start3)
 
 
 async def test_queries():
     start1 = time()
     if await connect_to_prisma(prisma):
-        start2 = datetime.now()
         await prisma.audit_log.find_many()
-        start3 = datetime.now()
         print("Test Query 1", time()-start1)
-        # print("Query 2", start2)
-        # print("Query 3", start3)
 
 
 if __name__ == '__main__':
     # asyncio.run(main())
     # asyncio.run(main_queries())
     # asyncio.run(batcher())
+    asyncio.run(batcher_function())
     # asyncio.run(batcher_queries())
-    asyncio.run(test())
-    asyncio.run(test_queries())
+    # asyncio.run(test())
+    # asyncio.run(test_queries())
